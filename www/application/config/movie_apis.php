786707cd41e2767fffb228d1a03427d0783d6058
Derek Stutsman <derek@stutsmansoft.com>
2014-03-24 17:47:25 -0500

    Add config for movie API keys

---
+++ www/application/config/movie_apis.php
@@ -0,0 +1,10 @@
+<?php
+	//Rotten Tomatoes API Key
+	$config['rotten_tomatoes_api_key'] = "yytapgjcae7pu4j42dwmmmg5";
+
+	//The Movie DB API
+	$config['tmdb_api_key'] = "1e0e9eb6323a33a3b940f6720a2915f1";
+
+	//TMS API KEY
+	$config['tms_api_key'] = "9a782k4scvuzsgye7xg9mfsh";	
+?>
