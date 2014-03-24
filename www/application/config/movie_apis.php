745eda2aa1de50500fabf7d33ab9abe3aa721a3d
Derek Stutsman <derek@stutsmansoft.com>
2014-03-24 18:54:22 -0500

    Set default cache timeouts for services. Add stub memcached.php config file.

--- www/application/config/movie_apis.php
+++ www/application/config/movie_apis.php
@@ -1,10 +1,13 @@
 <?php
 	//Rotten Tomatoes API Key
 	$config['rotten_tomatoes_api_key'] = "yytapgjcae7pu4j42dwmmmg5";
+	$config['rotten_tomatoes_cache_seconds'] = 60 * 60 * 24;
 
 	//The Movie DB API
 	$config['tmdb_api_key'] = "1e0e9eb6323a33a3b940f6720a2915f1";
+	$config['tmdb_cache_seconds'] = 60 * 60 * 24;	
 
 	//TMS API KEY
 	$config['tms_api_key'] = "9a782k4scvuzsgye7xg9mfsh";	
+	$config['tms_cache_seconds'] = 60 * 60 * 24;	
 ?>
