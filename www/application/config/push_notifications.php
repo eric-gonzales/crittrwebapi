20ef484c33a61232111d714f118c00b1d2f200cb
Derek Stutsman <derek@stutsmansoft.com>
2014-03-24 17:33:33 -0500

    Add APNS-PHP library. Add Critter push certs and add simple command line wrapper for sending (and emptying) push queue and calling Apple's feedback service.

---
+++ www/application/config/push_notifications.php
@@ -0,0 +1,7 @@
+<?php
+
+	$config['push_sandbox'] = FALSE;
+	$config['push_sandbox_cert'] = "Critter_Dev_Push.pem";
+	$config['push_production_cert'] = "Critter_Production_Push.pem";
+
+?>
---
+++ www/application/controllers/push.php
@@ -0,0 +1,45 @@
+<?php
+class Push extends CI_Controller 
+{
+    function __construct() 
+    {
+        parent::__construct();
+		$this->load->model('notification_model');
+    }
+    
+	public function feedback()
+	{
+		foreach($this->notification_model->feedback() as $feedbackToken)
+		{
+			$token = $feedback["deviceToken"];
+			$this->db->where("push_token", $token);
+			$this->db->set("push_token", NULL);
+			$this->db->update("CRDevice");
+		}
+	}
+	
+	public function send()
+	{
+		$this->db->select('CRPushNotification.id, CRDevice.push_token');
+		$this->db->from('CRPushNotification');
+		$this->db->join('CRDevice', 'CRDevice.id = CRPushNotification.device_id');
+		$this->db->where('sent', FALSE);
+		$this->db->where('CRDevice.push_token is not null');
+		$query = $this->db->get();
+		
+		foreach($query->result as $row)
+		{
+			$this->notification_model->send_ios($row->push_token, $row->message, intval($row->badge));
+			
+			$this->db->where('id', $row->id);
+			$this->db->set('sent', TRUE);
+			$this->db->update('CRPushNotification');
+		}
+	}
+	
+	public function test($token, $message)
+	{
+		$this->notification_model->send_ios($token, $message);
+	}
+}
+?>
