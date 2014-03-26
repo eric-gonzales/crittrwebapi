d98b3b5d851b5d721c2f44092c79e4b7f7c12f16
Eric Gonzales <guardians611@gmail.com>
2014-03-26 19:18:22 -0400

    Begin user controller

---
+++ www/application/controllers/user.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * User Controller
+ * @author Eric Gonzales <eric@crittermovies.com>
+ * @copyright 2014 Critter
+ */
+
+class User extends CI_Controller{
+	function index(){}
+	
+	//Create new Account
+	function signup(){}
+	
+	//Login or Create New Account via Facebook
+	function facebook(){}
+	
+	//Login
+	function login(){}
+	
+	//Reset Lost Password
+	function reset(){}
+	
+	//Update User Profile Photo
+	function photo(){}
+	
+	//Add Friend
+	function addfriend(){}
+	
+	//Remove Friend
+	function removefriend(){}
+	
+}
