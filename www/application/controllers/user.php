bbf5bb83f1d050fe5d8e0b99b72af06f81281d43
Eric Gonzales <guardians611@gmail.com>
2014-03-27 13:30:25 -0400

    Revert last changes

--- www/application/controllers/user.php
+++ www/application/controllers/user.php
@@ -6,14 +6,12 @@
  */
 
 class User extends CI_Controller{
-	function __construct(){
-		$this->load->model('user_model');
-	}
-	
 	function index(){}
 	
 	//Create new Account
 	function signup(){
+		$this->load->model('user_model');
+		
 		//first we check if the email is already in use
 		$chk_stmt = $this->db->get_where('CRUser',array('email' => $this->input->post('email')), 1);
 		
@@ -66,7 +64,9 @@ class User extends CI_Controller{
 	function facebook(){}
 	
 	//Login
-	function login(){		
+	function login(){
+		$this->load->model('user_model');
+				
 		$this->db->select('id, password_hash');
 		//look for matching email in CRUser table
 		$chk_stmt = $this->db->get_where('CRUser',array('email' => $this->input->post('email')), 1);
