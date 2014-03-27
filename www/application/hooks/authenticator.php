963427a7018bdb17f6cf23a53e5e5435ef21e64e
Eric Gonzales <guardians611@gmail.com>
2014-03-26 22:41:14 -0400

    Auth use shared secret

--- www/application/hooks/authenticator.php
+++ www/application/hooks/authenticator.php
@@ -9,10 +9,9 @@ class Authenticator{
 			$this->fail();
 		}
 		
-		define('CRITTER_SECRET', 'p0^:22MJQ4OkR6235w2+');
 		$critterDevice = $headers['critter-device'];
 		$headerVerify = $headers['critter-verify'];
-		$test = sha1($critterDevice.CRITTER_SECRET);
+		$test = sha1($critterDevice.$this->config->item('shared_secret'));
 		
 		//if the hash doesn't match up or the critter-device header is empty
 		if($headerVerify != $test || empty($critterDevice)){
