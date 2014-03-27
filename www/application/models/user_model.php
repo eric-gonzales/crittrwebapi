5119075a0eeaa820849fb779a46d628156567ebb
Eric Gonzales <guardians611@gmail.com>
2014-03-26 23:20:04 -0400

    User login WIP: authentication

--- www/application/models/user_model.php
+++ www/application/models/user_model.php
@@ -89,9 +89,6 @@ class User_model extends CR_Model {
 		else{
 			$cr_user = $chk_stmt->result();
 			$hashedPassword = $this->hash($_POST['password']);
-			echo $hashedPassword;
-			echo '<br>';
-			echo $cr_user->password_hash;
 			if($hashedPassword == $cr_user[0]->password_hash){
 				$this->defaultResult($userID);
 			}
