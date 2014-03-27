5ab1af5d6b721c3caa3a165eb97923f192032a23
Eric Gonzales <guardians611@gmail.com>
2014-03-27 14:53:15 -0400

    Password reset: expired token WIP

--- www/application/controllers/user.php
+++ www/application/controllers/user.php
@@ -114,12 +114,10 @@ class User extends CI_Controller{
 		else{
 			$cr_user = $chk_stmt->row();
 			//check if token is already generated for this user_id. 
-			$this->db->select('((created + INTERVAL 7 DAY) > NOW())');
+			$this->db->select('((created + INTERVAL 7 DAY) < NOW())');
 			$chk_tkn_stmt = $this->db->get_where('CREmailToken',array('user_id' => $cr_user->id), 1);
 			if($chk_tkn_stmt->num_rows() > 0){
 				$tkn = $chk_tkn_stmt->row();
-				echo '<pre>';
-				print_r($tkn);
 				//return error code
 				$this->user_model->setStatus(1);
 				$this->user_model->setMessage('Error: token has already been generated for this email or is expired.');	
