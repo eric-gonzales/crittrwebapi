65fba0d78ef6a267015d569dc2b89cab3bd9ba9a
Eric Gonzales <guardians611@gmail.com>
2014-03-26 23:38:50 -0400

    User login WIP: authentication

--- www/application/models/user_model.php
+++ www/application/models/user_model.php
@@ -120,7 +120,7 @@ class User_model extends CR_Model {
 	
 	//Hash
 	private function hash($string){
-		echo $string;
+		echo sha1(hashids_encrypt($string, base64_encode($this->config->item('server_secret')), 10));
 		return sha1(hashids_encrypt($string, base64_encode($this->config->item('server_secret')), 10));
 	}
 }
