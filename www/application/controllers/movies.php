e5e3eb48578a790822049dd9dde36e8631425d90
Eric Gonzales <guardians611@gmail.com>
2014-03-28 18:11:28 -0400

    Add Movies Controller and Model

---
+++ www/application/controllers/movies.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Movies Controller
+ * @author Eric Gonzales <eric@crittermovies.com>
+ * @copyright 2014 Critter
+ */
+
+class Movies extends CI_Controller{
+	function __construct(){
+		parent::__construct();
+		$this->load->model('movies_model');
+	}
+	
+	//Fetch Priority Movies
+	public function priority($hashedUserID){}
+	
+	//Fetch Unrated Movies for User
+	public function unrated($hashedUserID){}
+	
+	//Fetch Box Office Movies for User
+	public function boxoffice($hashedUserID, $limit, $countryCode){}
+	
+	//Fetch Opening Movies for User
+	public function opening($hashedUserID, $limit, $countryCode){}
+	
+	//Fetch Upcoming Movies for User
+	public function upcoming($hashedUserID, $limit, $page, $countryCode){}
+	
+	//Fetch New Release DVDs for User
+	public function newreleasedvds($hashedUserID, $limit, $page, $countryCode){}
+	
+	//Fetch Current Release DVDs for User
+	public function currentdvds($hashedUserID, $limit, $page, $countryCode){}
+	
+	//Fetch Upcoming DVDs for User
+	public function upcomingdvds($hashedUserID, $limit, $page, $countryCode){}
+	
+	//Movie Search
+	public function search($searchTerm, $limit, $page){}
+	
+	//Generate Error
+	public function _generateError($message, $status = 1){
+		$this->movies_model->setStatus($status);
+		$this->movies_model->setMessage('Error: '.$message);
+	}
+	
+	//Produce Response
+	public function _response(){
+		$data['status'] = $this->movies_model->getStatus();
+		$data['message'] = $this->movies_model->getMessage();
+		$data['result'] = $this->movies_model->getResult();
+		$this->load->view('standard_response', $data);
+	}
+}
---
+++ www/application/models/movies_model.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Movies Model
+ * @author Eric Gonzales <eric@crittermovies.com>
+ * @copyright 2014 Critter
+ */
+ 
+class Movies_model extends CR_Model {
+
+	/**
+     * Default constructor
+     * @param void
+     * @return void
+     * @access public 
+     */
+	public function __construct(){
+		parent::__construct();
+	}
+}
