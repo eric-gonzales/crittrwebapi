70a9827caf0b9fd2e5ad8378622c0e1210b150ef
Eric Gonzales <guardians611@gmail.com>
2014-03-28 20:41:03 -0400

    Memcached config update

--- www/application/config/memcached.php
+++ www/application/config/memcached.php
@@ -1,9 +1,7 @@
 <?php
 
 $config['memcached'] = array(
-	'default' => array(
 	    'hostname' => 'critter.yqt64u.cfg.usw2.cache.amazonaws.com',
 	    'port' => 11211,
 	    'weight' => 1
-    )
 ); 
