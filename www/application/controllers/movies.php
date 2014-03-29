5e7ccf9887818361287f7bcdd3603af293ad9225
Eric Gonzales <guardians611@gmail.com>
2014-03-28 23:41:08 -0400

    Movie search debugging

--- www/application/controllers/movies.php
+++ www/application/controllers/movies.php
@@ -71,15 +71,15 @@ class Movies extends CI_Controller{
 				$r['imdb_id'] = $rt_res->alternate_ids->imdb;
 			}
 			else{
-				$omdb_url = sprintf($this->config->item('omdb_title_url'), $r['title']);
+			/*	$omdb_url = sprintf($this->config->item('omdb_title_url'), $r['title']);
 				$omdb_info = $this->_getCachedData($omdb_url, $this->config->item('omdb_cache_seconds'));
 				$omdb_res = json_decode($omdb_info);
 				if(!empty($omdb_res)){
 					$r['imdb_id'] = $omdb_res->imdbID;
 				}
-				else{
+				else{*/
 					$r['imdb_id'] = '';
-				}
+				//}
 			}
 			if(!empty($r)){
 				array_push($results, $r);
