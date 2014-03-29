5a76357d0d5aa0c25544ef2408f605d7902ffd2c
Eric Gonzales <guardians611@gmail.com>
2014-03-28 22:25:55 -0400

    Movie search: for results, use object notation instead of array

--- www/application/controllers/movies.php
+++ www/application/controllers/movies.php
@@ -58,13 +58,11 @@ class Movies extends CI_Controller{
 			$rt_url = sprintf($this->config->item('rotten_tomatoes_movie_url'), $results['rotten_tomatoes_id'], $this->config->item('rotten_tomatoes_api_key'));
 			$rt_info = $this->_getCachedData($rt_url, $this->config->item('rotten_tomatoes_cache_seconds'));
 			$rt_res = json_decode($rt_info);
-			$results['title'] = $rt_res['title'];
-			$results['hashtag'] = '#'.str_replace(' ', '', strtolower($rt_res['title']));
-			$results['box_office_release_date'] = $rt_res['release_dates']['theater'];
-			$results['dvd_release_date'] = $rt_res['release_dates']['dvd'];
-			$results['imdb_id'] = $rt_res['alternate_ids']['imdb'];
-			
-			
+			$results['title'] = $rt_res->title;
+			$results['hashtag'] = '#'.str_replace(' ', '', strtolower($rt_res->title));
+			$results['box_office_release_date'] = $rt_res->release_dates->theater;
+			$results['dvd_release_date'] = $rt_res->release_dates->dvd;
+			$results['imdb_id'] = $rt_res->alternate_ids->imdb;
 		}
 		
 		$this->movies_model->setResult($results);
