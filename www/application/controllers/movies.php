abeeb06c0fc10a188a71f60dab9dcd74d00e4b70
Eric Gonzales <guardians611@gmail.com>
2014-03-28 21:38:12 -0400

    Movie search debugging

--- www/application/controllers/movies.php
+++ www/application/controllers/movies.php
@@ -59,7 +59,7 @@ class Movies extends CI_Controller{
 		
 		$response = json_decode($movie_info);
 		$movies = $response['movies'];
-		foreach($movies as $movie){
+	/*	foreach($movies as $movie){
 			//get RT details using RT ID:
 			$results['rotten_tomatoes_id'] = $movie->id;
 			$rt_url = sprintf($this->config->item('rotten_tomatoes_movie_url'), $results['rotten_tomatoes_id'], $this->config->item('rotten_tomatoes_api_key'));
@@ -81,7 +81,7 @@ class Movies extends CI_Controller{
 			$results['imdb_id'] = $rt_res['alternate_ids']['imdb'];
 			
 			
-		}
+		}*/
 		
 		$this->movies_model->setResult($results);
 		$this->_response();
