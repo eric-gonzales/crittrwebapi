27eb64180cf2d67d591fe52f80606e79a29b0c48
Eric Gonzales <guardians611@gmail.com>
2014-03-31 10:18:52 -0400

    Init: check for required post fields, fail if any are missing

--- www/application/controllers/init.php
+++ www/application/controllers/init.php
@@ -6,15 +6,34 @@
  */
 
 class Init extends CI_Controller{
-	function index(){
+	function __construct(){
+		parent::__construct();
 		$this->load->model('init_model');
+	}
+	
+	//Initalization
+	function index(){
+		if($this->input->post('appID') != '' && $this->input->post('appName') != '' && $this->input->post('appVersion') != '' && $this->input->post('deviceID') != ''){
+			$this->init_model->process();	
+		}
+		else{
+			$this->_generateError('required fields missing');
+		}
 		
-		$this->init_model->process();
-		
+		$this->_response();
+	}
+	
+	//Generate Error
+	public function _generateError($message, $status = 1){
+		$this->init_model->setStatus($status);
+		$this->init_model->setMessage('Error: '.$message);
+	}
+	
+	//Produce Response
+	public function _response(){
 		$data['status'] = $this->init_model->getStatus();
 		$data['message'] = $this->init_model->getMessage();
 		$data['result'] = $this->init_model->getResult();
-		
 		$this->load->view('standard_response', $data);
 	}
 }
