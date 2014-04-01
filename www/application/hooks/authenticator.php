316092042b56384d5fec1e4461e0fc5b149a7c54
Eric Gonzales <guardians611@gmail.com>
2014-03-31 23:31:48 -0400

    Disable authentication hook for CLI PHP

--- www/application/hooks/authenticator.php
+++ www/application/hooks/authenticator.php
@@ -4,22 +4,19 @@ class Authenticator{
 	 * Method used for authentication
 	 */
 	public function auth(){
-		
-		define('CRITTER_SECRET', 'ff8347fskjqd823e2dh2fds');
-		
-		$headers = getallheaders();
-		if(empty($headers)){
-			$this->fail();
-		}
-		
-		$critterDevice = $headers['critter-device'];
-		$headerVerify = $headers['critter-verify'];
-		
-		$test = sha1($critterDevice.CRITTER_SECRET);
-		
-		//if the hash doesn't match up or the critter-device header is empty
-		if($headerVerify != $test || empty($critterDevice)){
-			$this->fail();
+		if(php_sapi_name() != 'cli'){
+			define('CRITTER_SECRET', 'ff8347fskjqd823e2dh2fds');
+			$headers = getallheaders();
+			if(empty($headers)){
+				$this->fail();
+			}
+			$critterDevice = $headers['critter-device'];
+			$headerVerify = $headers['critter-verify'];
+			$test = sha1($critterDevice.CRITTER_SECRET);
+			//if the hash doesn't match up or the critter-device header is empty
+			if($headerVerify != $test || empty($critterDevice)){
+				$this->fail();
+			}
 		}
 	}
 	
