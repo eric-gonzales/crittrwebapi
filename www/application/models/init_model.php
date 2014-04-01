12948e22a540e130b0620a68b6a31d158c7c6768
Eric Gonzales <guardians611@gmail.com>
2014-03-31 23:12:14 -0400

    Init Model: json fix

--- www/application/models/init_model.php
+++ www/application/models/init_model.php
@@ -14,19 +14,20 @@ class Init_model extends CR_Model {
      */
 	public function __construct(){
 		parent::__construct();
+		$this->post = json_decode(file_get_contents('php://input'));
 	}
 	
 	public function process(){
 		//first we check if the device exists.
-		$chk_stmt = $this->db->get_where('CRDevice',array('device_vendor_id' => $this->input->post('deviceID')), 1);
+		$chk_stmt = $this->db->get_where('CRDevice',array('device_vendor_id' => $this->post->deviceID), 1);
 		
 		//if the device does not exist in the DB, we will add an entry
 		if($chk_stmt->num_rows() == 0){
 			$this->db->set('created', 'NOW()', FALSE);
-			$this->db->set('appID', $this->input->post('appID'));
-			$this->db->set('appName', $this->input->post('appName'));
-			$this->db->set('appVersion',$this->input->post('appVersion'));
-			$this->db->set('device_vendor_id', $this->input->post('deviceID'));
+			$this->db->set('appID', $this->post->appID);
+			$this->db->set('appName', $this->post->appName);
+			$this->db->set('appVersion',$this->post->appVersion);
+			$this->db->set('device_vendor_id', $this->post->deviceID);
 			$this->db->insert('CRDevice');
 		}
 		
