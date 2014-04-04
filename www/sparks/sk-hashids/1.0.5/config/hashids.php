d104db5120cd6868f551a53b40fcbef32f84a715
Derek Stutsman <derek@stutsmansoft.com>
2014-04-04 18:14:21 -0500

    Increase the width of hashed IDs and add a salt

--- www/sparks/sk-hashids/1.0.5/config/hashids.php
+++ www/sparks/sk-hashids/1.0.5/config/hashids.php
@@ -14,11 +14,11 @@
 // Custom value that will make your hashes unique to your salt.
 // If salt is changed, your hashes cannot be decrypted properly.
 // Empty string by default.
-$config['hashids_salt']             = '';
+$config['hashids_salt']             = 'Critwok';
 
 // Minimum hash length to set for your hashes. Default is 0,
 // meaning your hashes will be the shortest possible.
-$config['hashids_min_hash_length']  = 0;
+$config['hashids_min_hash_length']  = 10;
 
 // Custom alphabet to set for your hashes. By default it's set
 // to lower case letters, upper case letters, and numbers.
