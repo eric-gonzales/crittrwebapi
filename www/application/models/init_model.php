d59942728eacb7e94fec16dade1bbd9573fa0013
Derek Stutsman <derek@stutsmansoft.com>
2014-04-09 02:21:41 -0500

    Fix device registration

--- www/application/models/init_model.php
+++ www/application/models/init_model.php
@@ -17,20 +17,33 @@ class Init_model extends CR_Model {
 		$this->post = json_decode(file_get_contents('php://input'));
 	}
 	
-	public function process(){
-		//first we check if the device exists.
-		$chk_stmt = $this->db->get_where('CRDevice',array('device_vendor_id' => $this->post->deviceID), 1);
-		if($chk_stmt->num_rows() == 0){
-			//if the device does not exist in the DB, we will add an entry
-			$this->db->set('created', 'NOW()', FALSE)->set('appID', $this->post->appID)->set('appName', $this->post->appName)->set('appVersion',$this->post->appVersion)->set('device_vendor_id', $this->post->deviceID);
-			$this->db->set('modified', 'NOW()', FALSE);									
+	public function process()
+	{
+		//Get the device if present
+		$deviceVendorID = $this->post->deviceID;
+		$this->db->from('CRDevice');
+		$this->db->where('device_vendor_id',$deviceVendorID);
+		$row = $this->db->get()->row();
+
+		//Update the fields
+		$this->db->set('appID', $this->post->appID);
+		$this->db->set('appName', $this->post->appName);
+		$this->db->set('appVersion', $this->post->appVersion);
+		$this->db->set('modified', 'NOW()', FALSE);
+		if(!$row)
+		{
+			//Insert
+			$this->db->set('device_vendor_id', $deviceVendorID);
+			$this->db->set('created', 'NOW()', FALSE);
 			$this->db->insert('CRDevice');
 		}
-		else{
-			//if it does exist, we will update the modified field
-			$this->db->where('device_vendor_id', $this->post->deviceID)->set('modified', 'NOW()', FALSE);
+		else
+		{
+			//Update
+			$this->db->where('id', $row->id);
 			$this->db->update('CRDevice');
 		}
+		
 		//our result payload
 		$this->setResult(array(
 			'url_newaccount' => $this->config->item('base_url').'user/signup',
