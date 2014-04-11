c6363b9e2bc5185df3ee8ac5d558d5c388ec938d
Derek Stutsman <derek@stutsmansoft.com>
2014-04-11 09:47:15 -0500

    Update warmcache script to match new boxoffice API call

--- www/warmcache.sh
+++ www/warmcache.sh
@@ -1,6 +1,6 @@
 cd /var/www/html/www
 echo "Box Office $1"
-php index.php movies boxoffice jR7Y2WLmzo 50 $1 > /dev/null
+php index.php movies boxoffice 50 $1 > /dev/null
 echo "Opening $1"
 php index.php movies opening jR7Y2WLmzo 50 $1 > /dev/null
 echo "Upcoming $1"
