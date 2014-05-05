4582d7dec87a7a53b16afe5b4dfea113a06b365a
Derek Stutsman <derek@stutsmansoft.com>
2014-05-05 17:17:20 -0500

    Update push notification cert

--- www/application/config/push_notifications.php
+++ www/application/config/push_notifications.php
@@ -2,6 +2,6 @@
 
 	$config['push_sandbox'] = FALSE;
 	$config['push_sandbox_cert'] = "Critter_Dev_Push.pem";
-	$config['push_production_cert'] = "Critter_Production_Push.pem";
+	$config['push_production_cert'] = "CritterMedia_Production_Push.pem";
 
 ?>
