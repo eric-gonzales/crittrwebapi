e584ed4fadf21e45bc114beeb4bcdf0e81646c7f
Derek Stutsman <derek@stutsmansoft.com>
2014-07-13 13:45:38 -0500

    Convert server to use production push on AppStore cert.

--- www/application/config/push_notifications.php
+++ www/application/config/push_notifications.php
@@ -2,7 +2,7 @@
 
 	$config['push_sandbox'] = FALSE;
 	$config['push_sandbox_cert'] = "Critter_Dev_Push.pem";
-	$config['push_production_cert'] = "CritterMedia_Production_Push.pem";		//Disable this to go live
-	//$config['push_production_cert'] = "Crittr_Production_Push.pem";			//Enable this to go live
+	//$config['push_production_cert'] = "CritterMedia_Production_Push.pem";		//Enterprise cert
+	$config['push_production_cert'] = "Crittr_Production_Push.pem";			//App Store cert
 
 ?>
