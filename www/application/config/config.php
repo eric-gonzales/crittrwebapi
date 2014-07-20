530ee9d5c12bf8223701b1bf455e532dfe9fe22d
Derek Stutsman <derek@stutsmansoft.com>
2014-07-19 22:42:25 -0500

    Limit https to the initial authentication hit only

--- www/application/config/config.php
+++ www/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'https://api.crittermovies.com/';
+$config['base_url']	= 'http://api.crittermovies.com/';
 
 /*
 |--------------------------------------------------------------------------
